c66a57f0324686e1f1e41c14227d136fd4dbb13a
Full Name <Michael>
2021-07-24 13:13:48 -0400

    working on creating test for my ecommerse

---
+++ small_projects/ecommerce/test_BikeRental.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestBikeRental(TestCase):
+    def test_displaystock(self):
+        self.fail()
+
+    def test_rent_bike_on_hour(self):
+        self.fail()
+
+    def test_rent_bike_on_daily(self):
+        self.fail()
+
+    def test_rent_bike_on_week(self):
+        self.fail()
+
+    def test_return_bike(self):
+        self.fail()
